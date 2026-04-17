353d765b5478ba9627dad4c2cfd7fb1235c05741
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2026-04-17 22:43:51 +0200

    Add XML documentation to IPollingCommand interface

--- src/Ossendorf.Csla.PollingCommand/IPollingCommand.cs
+++ src/Ossendorf.Csla.PollingCommand/IPollingCommand.cs
@@ -3,9 +3,39 @@ using Ossendorf.Csla.PollingCommand.Client;
 
 namespace Ossendorf.Csla.PollingCommand;
 
+/// <summary>
+/// Defines a mechanism for executing long running commands with polling.
+/// </summary>
 public interface IPollingCommand {
+    /// <summary>
+    /// Executes the <typeparamref name="T"/> command without any parameters.
+    /// </summary>
+    /// <typeparam name="T">Command to execute.</typeparam>
+    /// <returns>The task object representing the asynchronous operation.</returns>
     Task<T> Execute<T>() where T : CommandBase<T>;
+
+    /// <summary>
+    /// Executes the <typeparamref name="T"/> command without any parameters.
+    /// </summary>
+    /// <typeparam name="T">Command to execute.</typeparam>
+    /// <param name="options">The options to configure per execution options.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
     Task<T> Execute<T>(PollingOptions options) where T : CommandBase<T>;
+
+    /// <summary>
+    /// Executes the <typeparamref name="T"/> command wit the given parameters.
+    /// </summary>
+    /// <typeparam name="T">Command to execute.</typeparam>
+    /// <param name="executeParameters">The parameters for the execute method. Remark: Make sure only to pass objects which are serializable by csla.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
     Task<T> Execute<T>(params object[] executeParameters) where T : CommandBase<T>;
+
+    /// <summary>
+    /// Executes the <typeparamref name="T"/> command wit the given parameters.
+    /// </summary>
+    /// <typeparam name="T">Command to execute.</typeparam>
+    /// <param name="options">The options to configure per execution options.</param>
+    /// <param name="executeParameters">The parameters for the execute method. Remark: Make sure only to pass objects which are serializable by csla.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
     Task<T> Execute<T>(PollingOptions options, params object[] executeParameters) where T : CommandBase<T>;
 }
